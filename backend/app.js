2e55b278a26837a65946ec7e07c86222040aafc2
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-03-07 02:57:33 +0530

    Refactor app.js to enhance middleware setup and route mounting

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,18 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(express.static("/public"))
+app.use(cookieParser())
 
+// Routes
 import router from "./src/modules/auth/auth.routes.js";
+import productRouter from "./src/modules/product/product.routes.js";
+// Mount the  routes
 app.use("/api/v1/auth" , router)
+app.use("/api/v1/product" , productRouter)
 
 export {app}
