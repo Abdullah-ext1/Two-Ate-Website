fdd40c7f0338bd693b09e26317aef577d69e635d
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-03-08 02:48:45 +0530

    Add cart model and integrate cart routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,10 @@ app.use(cookieParser())
 // Routes
 import router from "./src/modules/auth/auth.routes.js";
 import productRouter from "./src/modules/product/product.routes.js";
+import cartRouter from "./src/modules/cart/cart.routes.js";
 // Mount the  routes
 app.use("/api/v1/auth" , router)
 app.use("/api/v1/product" , productRouter)
+app.use("/api/v1/cart" , cartRouter)
 
 export {app}
---
+++ backend/src/modules/cart/cart.models.js
@@ -0,0 +1,30 @@
+import mongoose , {Schema} from "mongoose";
+ 
+const cartSchema = new Schema({
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "User" 
+    },
+    items: [{
+        product: {
+            type : mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true
+        },
+        size: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        priceSnapshot: {
+            type: Number,
+            required: true
+        }
+
+}]
+})
+
+export const Cart = mongoose.model("Cart" , cartSchema)
